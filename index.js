a814c16f5dad24efacc30cc2cdbd492fe3faf7ae
Max Anderberg <maxanderberg@gmail.com>
2021-04-22 10:52:43 +0200

    added some comments for clarity

--- index.js
+++ index.js
@@ -7,19 +7,24 @@ const PORT = 8080;
 
 
 http.createServer((req, res) => {
+    // get the file path based on the requested URL
     const filePath = path.join(
         __dirname,
         'public', 
         req.url === '/' ? 'index.html' : req.url)
 
+    // initialize content type
     let contentType = 'text/html';
 
+    // get the requested file's extension name
     const extName = path.extname(filePath);
-    
+
+    // if the extension name is .css, set the content type to css
     if(extName === '.css'){
         contentType = 'text/css';
     }
 
+    // read the file given the file path
     fs.readFile(filePath, (error, data) => {
     
         if(error) {
